3a65bd9dc01adb34e9f069695659d109f2789f74
aiuhjv1 <43039187+ajisrael@users.noreply.github.com>
2023-04-24 23:17:28 -0400

    f   - query endpoint for getting item summary

---
+++ src/main/java/control/tower/api/query/inventoryItemSummary/InventoryItemSummaryController.java
@@ -0,0 +1,31 @@
+package control.tower.api.query.inventoryItemSummary;
+
+import control.tower.core.FindInventoryItemSummariesQuery;
+import control.tower.core.InventoryItemSummary;
+import org.axonframework.messaging.responsetypes.ResponseTypes;
+import org.axonframework.queryhandling.QueryGateway;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path="api/v1/inventoryItemSummary")
+public class InventoryItemSummaryController {
+    private final QueryGateway queryGateway;
+
+    public InventoryItemSummaryController(QueryGateway queryGateway) {
+        this.queryGateway = queryGateway;
+    }
+
+    @GetMapping
+    public List<InventoryItemSummary> getInventoryItemSummaries(@RequestParam(required = false, defaultValue = "0") int offset,
+                                                                @RequestParam(required = false, defaultValue = "100") int limit) {
+        return queryGateway.query(
+                new FindInventoryItemSummariesQuery(offset, limit),
+                ResponseTypes.multipleInstancesOf(InventoryItemSummary.class)
+        ).join();
+    }
+}
