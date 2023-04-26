e0c61bc145cc99397dff82b644125be3e757dcb7
aiuhjv1 <43039187+ajisrael@users.noreply.github.com>
2023-04-26 10:28:31 -0400

    r   - added error handling to createInventoryItem endpoint

--- src/main/java/control/tower/api/command/inventoryItem/InventoryItemController.java
+++ src/main/java/control/tower/api/command/inventoryItem/InventoryItemController.java
@@ -1,11 +1,17 @@
 package control.tower.api.command.inventoryItem;
 
 import control.tower.api.command.inventoryItem.models.InventoryItemRequestBody;
+import control.tower.api.command.inventoryItem.models.InventoryItemResponse;
 import control.tower.api.command.inventoryItem.models.MoveInventoryItemRequestBody;
 import control.tower.core.commands.CreateInventoryItemCommand;
 import control.tower.core.commands.MoveInventoryItemCommand;
+import org.axonframework.commandhandling.CommandExecutionException;
 import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping(path="api/v1/inventoryItem")
@@ -18,18 +24,31 @@ public class InventoryItemController {
     }
 
     @PostMapping
-    public void createInventoryItem(@RequestBody InventoryItemRequestBody inventoryItemRequestBody) {
-        // TODO: add try catch for proper error handling and notification to client upon error receipt
-        commandGateway.sendAndWait(
-                new CreateInventoryItemCommand(
-                        inventoryItemRequestBody.getSku(),
-                        inventoryItemRequestBody.getLocationId(),
-                        inventoryItemRequestBody.getBinId(),
-                        inventoryItemRequestBody.getName(),
-                        inventoryItemRequestBody.getPrice()
-                )
-        );
+    public ResponseEntity<Map<String, Object>> createInventoryItem(@RequestBody InventoryItemRequestBody inventoryItemRequestBody) {
+        try {
+            commandGateway.sendAndWait(
+                    new CreateInventoryItemCommand(
+                            inventoryItemRequestBody.getSku(),
+                            inventoryItemRequestBody.getLocationId(),
+                            inventoryItemRequestBody.getBinId(),
+                            inventoryItemRequestBody.getName(),
+                            inventoryItemRequestBody.getPrice()
+                    )
+            );
+            return ResponseEntity.ok(
+                    new InventoryItemResponse(true, "Inventory item created successfully")
+                            .getResponse());
+        } catch (IllegalArgumentException | CommandExecutionException e) {
+            return ResponseEntity.badRequest().body(
+                    new InventoryItemResponse(false, "Invalid argument: " + e.getMessage())
+                            .getResponse());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
+                    new InventoryItemResponse(false, "An error occurred: " + e.getMessage())
+                            .getResponse());
+        }
     }
+
 
     @PutMapping
     public void moveInventoryItem(@RequestBody MoveInventoryItemRequestBody moveInventoryItemRequestBody) {
