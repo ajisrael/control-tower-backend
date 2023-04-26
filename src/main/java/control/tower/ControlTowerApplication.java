41e930b5fe412348012cc0e4e32777bce7d3bd44
aiuhjv1 <43039187+ajisrael@users.noreply.github.com>
2023-04-26 11:58:44 -0400

    d   - added project todos

--- src/main/java/control/tower/ControlTowerApplication.java
+++ src/main/java/control/tower/ControlTowerApplication.java
@@ -8,4 +8,29 @@ public class ControlTowerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ControlTowerApplication.class, args);
     }
+
+    // TODO: Implement Inventory Item History view
+
+    // TODO: Create Database seeder
+
+    // TODO: Create Pick List Aggregate and Query Models
+
+    // TODO: Create Order Aggregate and Query Models
+
+    // TODO: Create Customer Aggregate and establish relationships with other entities:
+    //       - View all inventory items for a customer
+    //       - View all orders for a customer
+
+    // TODO: Create authentication endpoint for api with JWT
+
+    // TODO: Setup users and roles for access levels to endpoints
+
+    // TODO: Add additional properties to Inventory Items:
+    //       - boolean received
+    //       - boolean delivered
+    //       - Customer object?
+
+    // TODO: Create administration endpoint to view statistics of system
+
+    // TODO: Begin work on Front End
 }
