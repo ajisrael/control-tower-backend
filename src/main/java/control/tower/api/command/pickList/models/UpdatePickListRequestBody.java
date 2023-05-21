fd17b682921ecfaaa2fc94314b65849f5759bf10
aiuhjv1 <43039187+ajisrael@users.noreply.github.com>
2023-05-21 00:15:45 -0400

    r   - added lombok annotations to pick list api models

--- src/main/java/control/tower/api/command/pickList/models/UpdatePickListRequestBody.java
+++ src/main/java/control/tower/api/command/pickList/models/UpdatePickListRequestBody.java
@@ -1,45 +1,22 @@
 package control.tower.api.command.pickList.models;
 
-import java.util.Objects;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.ToString;
 
+@Getter
+@EqualsAndHashCode
+@ToString
 public class UpdatePickListRequestBody {
 
+    @NonNull
     private String pickId;
-
+    @NonNull
     private String sku;
 
     public UpdatePickListRequestBody(String pickId, String sku) {
         this.pickId = pickId;
         this.sku = sku;
     }
-
-    public String getPickId() {
-        return pickId;
-    }
-
-    public String getSku() {
-        return sku;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        UpdatePickListRequestBody that = (UpdatePickListRequestBody) o;
-        return Objects.equals(pickId, that.pickId) && Objects.equals(sku, that.sku);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(pickId, sku);
-    }
-
-    @Override
-    public String toString() {
-        return "PickInventoryItemRequestBody{" +
-                "pickId='" + pickId + '\'' +
-                ", sku='" + sku + '\'' +
-                '}';
-    }
-
 }
