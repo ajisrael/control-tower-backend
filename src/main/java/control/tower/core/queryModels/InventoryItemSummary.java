9a5cb654d7b1023a6b375d7ac1a8885a35468458
aiuhjv1 <43039187+ajisrael@users.noreply.github.com>
2023-05-21 00:02:25 -0400

    r   - added lombok annotations to queryModels

--- src/main/java/control/tower/core/queryModels/InventoryItemSummary.java
+++ src/main/java/control/tower/core/queryModels/InventoryItemSummary.java
@@ -1,28 +1,36 @@
 package control.tower.core.queryModels;
 
 import control.tower.core.valueObjects.Location;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import java.util.Objects;
 
 @Entity
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
 public class InventoryItemSummary {
 
     @Id
+    @NonNull
     private String sku;
-
+    @NonNull
     private String name;
-
+    @NonNull
     private String locationId;
-
+    @NonNull
     private String binId;
-
+    @NonNull
     private double price;
-
-    private String pickId = null;
-
+    @NonNull
     private boolean picked = false;
+    private String pickId = null;
 
     public InventoryItemSummary(String sku, String name, Location currentLocation, double price) {
         this.sku = sku;
@@ -33,88 +41,5 @@ public class InventoryItemSummary {
     }
 
     public InventoryItemSummary() {} // Required by Axon
-
-    public String getSku() {
-        return sku;
-    }
-
-    public void setSku(String sku) {
-        this.sku = sku;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLocationId() {
-        return locationId;
-    }
-
-    public void setLocationId(String locationId) {
-        this.locationId = locationId;
-    }
-
-    public String getBinId() {
-        return binId;
-    }
-
-    public void setBinId(String binId) {
-        this.binId = binId;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public String getPickId() {
-        return pickId;
-    }
-
-    public void setPickId(String pickId) {
-        this.pickId = pickId;
-    }
-
-    public boolean isPicked() {
-        return picked;
-    }
-
-    public void setPicked(boolean picked) {
-        this.picked = picked;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        InventoryItemSummary that = (InventoryItemSummary) o;
-        return Double.compare(that.price, price) == 0 && picked == that.picked && Objects.equals(sku, that.sku) && Objects.equals(name, that.name) && Objects.equals(locationId, that.locationId) && Objects.equals(binId, that.binId) && Objects.equals(pickId, that.pickId);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(sku, name, locationId, binId, price, pickId, picked);
-    }
-
-    @Override
-    public String toString() {
-        return "InventoryItemSummary{" +
-                "sku='" + sku + '\'' +
-                ", name='" + name + '\'' +
-                ", locationId='" + locationId + '\'' +
-                ", binId='" + binId + '\'' +
-                ", price=" + price +
-                ", pickId='" + pickId + '\'' +
-                ", picked=" + picked +
-                '}';
-    }
-
 }
 
--- src/main/java/control/tower/core/queryModels/PickListSummary.java
+++ src/main/java/control/tower/core/queryModels/PickListSummary.java
@@ -1,6 +1,10 @@
 package control.tower.core.queryModels;
 
 import control.tower.core.valueObjects.PickItemSummary;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.ToString;
 
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
@@ -9,18 +13,23 @@ import javax.persistence.Id;
 import javax.persistence.OrderBy;
 import java.util.Date;
 import java.util.List;
-import java.util.Objects;
 
 @Entity
+@Getter
+@EqualsAndHashCode
+@ToString
 public class PickListSummary {
 
     @Id
+    @NonNull
     private String pickId;
 
     @ElementCollection(fetch = FetchType.EAGER)
     @OrderBy("pick_item_sku DESC")
+    @NonNull
     private List<PickItemSummary> pickItemSummaryList;
 
+    @NonNull
     private Date pickDate;
 
     public PickListSummary(String pickId, List<PickItemSummary> pickItemSummaryList, Date pickDate) {
@@ -29,53 +38,5 @@ public class PickListSummary {
         this.pickDate = pickDate;
     }
 
-    public PickListSummary() {
-    }
-
-    public String getPickId() {
-        return pickId;
-    }
-
-    public void setPickId(String pickId) {
-        this.pickId = pickId;
-    }
-
-    public List<PickItemSummary> getPickItemSummaryList() {
-        return pickItemSummaryList;
-    }
-
-    public void setPickItemSummaryList(List<PickItemSummary> pickItemSummaryList) {
-        this.pickItemSummaryList = pickItemSummaryList;
-    }
-
-    public Date getPickDate() {
-        return pickDate;
-    }
-
-    public void setPickDate(Date pickDate) {
-        this.pickDate = pickDate;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        PickListSummary that = (PickListSummary) o;
-        return Objects.equals(pickId, that.pickId) && Objects.equals(pickItemSummaryList, that.pickItemSummaryList) && Objects.equals(pickDate, that.pickDate);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(pickId, pickItemSummaryList, pickDate);
-    }
-
-    @Override
-    public String toString() {
-        return "PickListSummary{" +
-                "pickId='" + pickId + '\'' +
-                ", pickItemSummaryList=" + pickItemSummaryList +
-                ", pickDate=" + pickDate +
-                '}';
-    }
-
+    public PickListSummary() {}
 }
