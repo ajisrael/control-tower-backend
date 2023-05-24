7cbf1716a3ca8b674ad389befc57494859952e5c
aiuhjv1 <43039187+ajisrael@users.noreply.github.com>
2023-05-23 22:30:43 -0400

    r   - fixed nonnull annotation on pick list deleted event

--- src/main/java/control/tower/core/events/PickListDeletedEvent.java
+++ src/main/java/control/tower/core/events/PickListDeletedEvent.java
@@ -2,16 +2,15 @@ package control.tower.core.events;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.ToString;
-
-import javax.annotation.Nonnull;
 
 @Getter
 @EqualsAndHashCode
 @ToString
 public class PickListDeletedEvent {
 
-    @Nonnull
+    @NonNull
     private final String pickId;
 
     public PickListDeletedEvent(String pickId) {
